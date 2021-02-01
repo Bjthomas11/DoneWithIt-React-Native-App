eed8fb90e1544b639c28ef7c1e796248815b7e50
Brian Thomas <bthomas@imaginuity.com>
2021-02-01 17:22:46 -0600

    setting up navigation

--- App.js
+++ App.js
@@ -1,8 +1,44 @@
 import React from "react";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import { Button, Text } from "react-native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import AppScreen from "./app/components/AppScreen/AppScreen";
+
+const Tweets = ({ navigation }) => (
+  <AppScreen>
+    <Text>Tweets</Text>
+    <Link />
+  </AppScreen>
+);
+
+const Link = () => {
+  const navigation = useNavigation();
+  return (
+    <Button title="Click" onPress={() => navigation.navigate("TweetDetails")} />
+  );
+};
+
+const TweetDetails = () => (
+  <AppScreen>
+    <Text>Tweet Details</Text>
+  </AppScreen>
+);
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen name="TweetDetails" component={TweetDetails} />
+  </Stack.Navigator>
+);
 
 const App = () => {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 };
 
 export default App;
